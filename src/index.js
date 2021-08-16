72329c7d1ef3fd290025c18e109e9f1760dc690d
Timur Salikhov <cruzemen@mail.ru>
2021-08-16 03:44:40 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = '';
+    for (let i = 0; i < expr.length; i+= 10){
+        let word = '';
+        let decode = expr.slice(i, i + 10);
+        if (decode != '**********') +decode;
+        while (decode) {
+            if (decode % 100 == 10) word += '.';
+            if (decode % 100 == 11) word += '-';
+            if (decode == '**********') { word += ' '; break; }
+            decode = Math.floor(decode / 100);
+        }
+        word = word.split('').reverse().join('');
+        word == ' ' ? result += ' ' : result += MORSE_TABLE[word];
+    }
+    return result;
+};
 
 module.exports = {
     decode
